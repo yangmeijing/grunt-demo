a9ccb72bdf7bbc6c9d3ef73743898d3ac2c8303b
yangmeijing <1806102644@qq.com>
2019-03-12 16:50:56 +0800

    sprite

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sprite:{
+      all: {
+        src: './images/*.png',
+        dest: './images/all.png',
+        destCss: './sprites.css'
+      }
+    }      
+  });
+
+    grunt.loadNpmTasks('grunt-spritesmith');
+
+    grunt.registerTask('default', ['sprite']);
+
+};
